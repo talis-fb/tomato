1a0f7feb2183a8fe842dac9e3e5d257ed10ebd46
Talison Fabio <talisonf11@gmail.com>
2022-01-12 16:31:17 -0300

    Tela Home e Header

--- boilerplate/src/modules/Sample/layouts/Main.js
+++ boilerplate/src/modules/Sample/layouts/Main.js
@@ -1,8 +1,18 @@
-import { Icon } from '@tomato/components'
+// import { Icon } from '@tomato/components'
+import Logo from '../assets/Logo.jpg'
 
 const styles ={
+    flex: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     imgs: {
         height: '100%',
+    },
+    fullSize: {
+        height: '100%',
+        width: '100%',
     }
 }
 
@@ -17,26 +27,32 @@ export const Main = ({ children, ...props }) => {
         }}>
             <header 
                 sx={{ 
+                    ...styles.flex,
+                    justifyContent: 'space-between',
                     position: 'relative',
-                    display: 'flex',
-                    justifyContent: "space-between",
                     width: '100%',
-                    height: '80px',
+                    height: '90px',
                     padding: '10px',
-                    backgroundColor: 'red',
+                    paddingLeft: '20px',
+                    paddingRight: '20px',
+                    backgroundColor: '#ffffff',
                     borderBottom: '2px solid grey',
+                    a:{
+                        textDecoration: 'none',
+                        color: '#111111'
+                    }
                 }}
             >
-                <div sx={{display: 'flex', justifyContent: 'center' }}>
-                    <a href="/"><img sx={styles.imgs} src="https://pipz.io/shared/1/files/logo_pipz_plataform.png" alt="" /></a>
+                <div sx={{ ...styles.flex, ...styles.imgs, gap:'20px' }}>
+                    <a href="/" sx={{ height: '100%' }} ><img sx={styles.imgs} src={ Logo } alt="" /></a>
 
-                    <a href="/shop" sx={styles.imgs}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="100%" fill="currentColor" viewBox="0 0 16 16"> <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/> </svg>
+                    <a href="/shop" sx={{...styles.imgs, ...styles.flex}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="30" fill="currentColor" viewBox="0 0 16 16"> <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/> </svg>
                         Shop
                     </a>
                 </div>
 
-                <div>
+                <div sx={{ ...styles.flex, ...styles.imgs}}>
                     <a href="/login"> 
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-square" viewBox="0 0 16 16"> <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/> <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/> </svg>
                         Signup/Login
--- boilerplate/src/modules/Sample/views/Home.js
+++ boilerplate/src/modules/Sample/views/Home.js
@@ -2,13 +2,70 @@
 // import { useSelector } from "react-redux";
 
 // import { Card, crud, Flex, Image, Spinner } from "@tomato/components";
-import { Card } from "@tomato/components";
+// import { Card } from "@tomato/components";
+// import Logo from '../assets/Logo.jpg'
+import styled from '@emotion/styled'
+
+const Button = styled.button({
+    border: 'none',
+    padding: '10px',
+    minHeight: '50px',
+    backgroundColor: '#555556',
+    color: '#ffffff',
+    borderRadius: '5px',
+    boxShadow: '1px solid black'
+})
 
 export const Home = () => {
     return (
-        <div>
-            <Card> Opa </Card>
-            <Card> Vinho dois</Card>
-        </div>
+        <>
+            <section sx={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+
+                <div sx={{ display: 'grid', maxWidth: '800px', gridTemplateColumns: '1fr 1fr' }} >
+                    <h2 sx= {{ fontSize: '30px' }}>SUAS SENSAÇÕES SÃO ÚNICAS. NÓS NÃO TE DEIXAMOS ESQUECER.</h2>
+                    <div>
+                        Crie agora a lista de suas melhores experiências
+                        <Button>Começar</Button>
+                    </div>
+                </div>
+
+            </section>
+
+            <section sx={{ width: '100%' }}>
+                <div sx={{ maxWidth: '800px',  margin: '0 auto', textAlign: 'center' }} >
+                    <h2 sx= {{ fontSize: '30px' }}>Como Funciona? </h2>
+                    <div sx={{ display: 'flex' }}>
+                        <div>
+                            <h3>Criando uma lista de Desejos</h3>
+                            <span>Escolha entre centenas de produtos e tendências escolhidas a dedo em nossa adega virtual.</span>
+                            <ul>
+                                <li>Procure os vinhos mais bem avaliados.</li>
+                                <li>Salve em seus favoritos na sua lista de desejos.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>Criando uma lista de Desejos</h3>
+                            <span>Escolha entre centenas de produtos e tendências escolhidas a dedo em nossa adega virtual.</span>
+                            <ul>
+                                <li>Procure os vinhos mais bem avaliados.</li>
+                                <li>Salve em seus favoritos na sua lista de desejos.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <Button>CRIE SEU WINE MENU</Button>
+                </div>
+            </section>
+
+            <section sx={{ width: '100%' }}>
+                    <h2 sx= {{ fontSize: '30px' }}>OS MAIS DESEJADOS</h2>
+                {/* <Vinhos /> */}
+            </section>
+
+            <section sx={{ width: '100%', padding: '20px', textAlign: 'center' }}>
+                    <div sx= {{ fontSize: '40px' }}>Comece sua lista de desejos</div>
+                    <Button>CRIE SEU WINE MENU</Button>
+                {/* <Vinhos /> */}
+            </section>
+        </>
     )
 };
